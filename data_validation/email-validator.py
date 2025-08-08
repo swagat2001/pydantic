6d5291da7ca83e6af68936b40320fbf8dd6b3b63
swagat2001 <swagatmohanty2018@gmail.com>
2025-08-09 00:01:23 +0530

    email-validatr

--- data_validation/email-validator.py
+++ data_validation/email-validator.py
@@ -1,2 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
+from typing import List,Dict
 
+class Patient(BaseModel):
+    name: str
+    email:EmailStr
+    age: int
+    Weight : float
+    married:bool
+    allergies:List[str]
+    contact_details: Dict[str,str]
+    
+def insert_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.email)
+    print(patient.age)
+    print(patient.Weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+        
+patient_info = {
+    'name': 'Swagat','email':'swag@gmail.com','age': 24,
+    'Weight': 65, 'married': False, 'allergies': ['dust'],
+    'contact_details': {'phone': '998877'}}
+    
+patient1 = Patient(**patient_info)
+insert_patient_data(patient1)
+    
