7b731627e4c5ea9dc72a73d2f2585327c3712302
swagat2001 <swagatmohanty2018@gmail.com>
2025-08-09 00:56:03 +0530

    use of fieldwit annoter-check line - 8

---
+++ data_validation/annotated-usecase.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import Dict,Annotated,Optional,List
+
+
+
+class Patient(BaseModel):
+    
+    name : Annotated[str, Field(max_length=50,title="Patient Name", description="Ensure name is not too long than 50 characters",examples=["Swagat Mohanty"])]  
+    email :EmailStr
+    Linkdin_URL : AnyUrl
+    age : int = Field(gt=0,lt=120)  # Ensure age is greater than 0
+    weight : float = Field(gt=0)  # Ensure weight is greater than 0
+    # Adding a field description for clarity)
+    allergies:Optional[List[str]] = Field(max_length=5)  # Ensure allergies list does not exceed 5 items
+    # Adding a field description for clarity
+    contact_details : Dict[str,str]
+
+
+def update_patient_data(patient:Patient):
+    print(patient.name)
+    print(patient.email)
+    print(patient.Linkdin_URL)
+    print(patient.weight)
+    print(patient.contact_details)
+    
+    
+patient_info = {'name':'Swagat','email':'swagat@gmail.com','Linkdin_URL':'http://linkedin.com/123'
+                ,'age': 24,'weight':65,
+                'contact_details':{'phone':'998877'}}
+patient1 = Patient(**patient_info)
+update_patient_data(patient1)
